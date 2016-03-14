3fbdecbdeb286733bb635b50c03e5d389ee058a0
James Owen <jamesowen012@gmail.com>
2016-03-14 15:29:21 +1100

    Basic Entity upload

---
+++ Tiles/Entity.cpp
@@ -0,0 +1,51 @@
+#include "Entity.h"
+
+Entity::Entity() : GameSprite() {
+
+}
+
+void Entity::MoveObject(float X, float Y) 
+{
+	setPosition(getPosition().x + X, getPosition().y + Y);
+}
+
+bool Entity::IsDead()
+{
+	return bIsDead;
+}
+
+void Entity::Damage(float damage) 
+{
+	if (IsDead() == false)
+	{
+		setHealth(getHealth() - damage);
+		if (health <= 0) 
+		{
+			bIsDead = true;
+			setHealth(0);
+		}
+	}
+}
+
+void Entity::Heal(float amount)
+{
+	if (IsDead() == false)
+	{
+		setHealth(getHealth() + amount);
+		if (health > maxHealth) 
+		{
+			setHealth(maxHealth);
+		}
+	}
+}
+
+void Entity::setHealth(float amount) 
+{
+	health = amount;
+}
+
+float Entity::getHealth() 
+{
+	return health;
+}
+
--- Tiles/Entity.h
+++ Tiles/Entity.h
@@ -5,9 +5,19 @@
 class Entity : public GameSprite
 {
 public:
+	Entity();
+	void MoveObject(float X, float Y);
 
+	bool IsDead();
+	void Damage(float damage);
+	void Heal(float amount);
+
+	void setHealth(float amount);
+	float getHealth();
 
 protected:
+	bool bIsDead = false;
 
-
+	float health;
+	float maxHealth;
 };
