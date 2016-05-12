ca01b7a32fc499b2285a0ed89d27f06aed46add1
unknown <hudoc96@hotmail.com>
2016-05-12 13:05:36 +1000

    forgot to add this to the last commit

--- Tiles/Main.cpp
+++ Tiles/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+#include "CollisionManager.h"
+
 #include "TextureHandler.h"
 #include "ImageHandler.h"
 #include "INIReader.h"
@@ -19,17 +21,17 @@ int main()
 		stoi(getValueFromFile("FrameHeight", "Default.ini"))),
 		"Tiles v" + getValueFromFile("CurrentVersion", "Default.ini"));
 
+	window.setKeyRepeatEnabled(false);
 	window.setVerticalSyncEnabled(StringToBool(getValueFromFile("V-syncEnabled", "Default.ini")));
 	window.setFramerateLimit(stoi(getValueFromFile("FPSLimit", "Default.ini")));
 
 	TextureHandler::instance()->setTexturePack("Default");
 	Menu TilesMenu;
 
+	TilesPlayer Player;
 	Board TilesBoard;
 	TilesBoard.CreateBoard();
-
-	TilesPlayer Player;
-	Player.setPosition(80, 15);
+	TilesBoard.PlacePlayer(&Player, 0, 0);
 
 	while (window.isOpen())
 	{
@@ -40,11 +42,31 @@ int main()
 			{
 				window.close();
 			}
+			if (event.type == sf::Event::KeyPressed)//&& Player..IsMoving() == false	//half works
+			{
+				if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up)
+				{
+					Player.Move("up");
+				}
+				if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down)
+				{
+					Player.Move("down");
+				}
+				if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left)
+				{
+					Player.Move("left");
+				}
+				if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right)
+				{
+					Player.Move("right");
+				}
+			}
 		}
 
 		//Updates
 		TilesMenu.Update(window);
 		Player.Update();
+		TilesBoard.Update();
 
 		//draws
 		window.clear(sf::Color::White);
